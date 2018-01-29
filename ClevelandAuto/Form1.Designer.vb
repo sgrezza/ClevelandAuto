a0de8fd04d8438d50161669cdc1831e22f1996ef
Spencer <sallegrezz@gmail.com>
2018-01-29 17:20:39 -0600

    Got my arrays. Need to collect properties and output them.

--- ClevelandAuto/Form1.Designer.vb
+++ ClevelandAuto/Form1.Designer.vb
@@ -22,9 +22,42 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnInformation = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 25
+        Me.ListBox1.Location = New System.Drawing.Point(72, 67)
+        Me.ListBox1.MultiColumn = True
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(784, 354)
+        Me.ListBox1.TabIndex = 0
+        '
+        'btnInformation
+        '
+        Me.btnInformation.Location = New System.Drawing.Point(361, 452)
+        Me.btnInformation.Name = "btnInformation"
+        Me.btnInformation.Size = New System.Drawing.Size(172, 62)
+        Me.btnInformation.TabIndex = 1
+        Me.btnInformation.Text = "Information"
+        Me.btnInformation.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(945, 545)
+        Me.Controls.Add(Me.btnInformation)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Two Dimensional Array"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btnInformation As Button
 End Class
--- ClevelandAuto/Form1.vb
+++ ClevelandAuto/Form1.vb
@@ -1,3 +1,39 @@
 ﻿Public Class Form1
+    Dim intCarCount, intPropertyCount As Integer
+    Dim strHeader = "CAR" & vbTab 'Header row 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnInformation.Click
+        'if input.yes then else...
+
+        'Ask user the car count and property count
+        intCarCount = InputBox("Please enter the number of cars you wish to enter")
+        intPropertyCount = InputBox("How many properties do you wish to enter?") - 1
+
+        Dim strProperties(intPropertyCount) As String
+        Dim strCars(intCarCount, intPropertyCount) As String
+
+        Dim counter = 0
+        For Each prop In strProperties 'Make the property header
+            counter += 1
+            strProperties(counter) = InputBox("Ok, enter property " & (counter)).ToString.ToUpper
+            strHeader += strProperties(counter) & vbTab
+        Next
+
+        ListBox1.Items.Add(strHeader)
+
+
+        'The first element of each array is the car name.
+        For car = 0 To intCarCount - 1
+            strCars(car, 0) = InputBox("Whats the name of car #" & car + 1 & "?")
+            For carProperty = 1 To intPropertyCount
+                strCars(car, carProperty) = InputBox("Enter the " & strCars(car, car "property for" & strCars(intCarCount, 0))
+            Next
+        Next
+        ListBox1.Items.Add(strCars(0, 1))
+        ListBox1.Items.Add(strCars(1, 1))
+
+
+
+
+    End Sub
 
 End Class
