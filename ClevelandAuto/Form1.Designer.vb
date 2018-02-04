ce04e2411f8af49f198042ecc2607ac10c2b6f3d
Spencer Allegrezza <sallegrezz@gmail.com>
2018-02-04 11:59:52 -0600

    Finished with Grades

--- ClevelandAuto/Form1.Designer.vb
+++ ClevelandAuto/Form1.Designer.vb
@@ -24,40 +24,94 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.btnInformation = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lstAverage = New System.Windows.Forms.ListBox()
+        Me.lstStudents = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'ListBox1
         '
+        Me.ListBox1.Font = New System.Drawing.Font("Inria Sans", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 25
+        Me.ListBox1.ItemHeight = 24
         Me.ListBox1.Location = New System.Drawing.Point(72, 67)
         Me.ListBox1.MultiColumn = True
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(784, 354)
+        Me.ListBox1.Size = New System.Drawing.Size(784, 340)
         Me.ListBox1.TabIndex = 0
         '
         'btnInformation
         '
-        Me.btnInformation.Location = New System.Drawing.Point(361, 452)
+        Me.btnInformation.Font = New System.Drawing.Font("Inria Sans", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInformation.Location = New System.Drawing.Point(506, 635)
         Me.btnInformation.Name = "btnInformation"
         Me.btnInformation.Size = New System.Drawing.Size(172, 62)
         Me.btnInformation.TabIndex = 1
-        Me.btnInformation.Text = "Information"
+        Me.btnInformation.Text = "Enter Grades"
         Me.btnInformation.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Inria Sans", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(684, 635)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(172, 62)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Close"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lstAverage
+        '
+        Me.lstAverage.Font = New System.Drawing.Font("Inria Sans", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstAverage.FormattingEnabled = True
+        Me.lstAverage.ItemHeight = 24
+        Me.lstAverage.Location = New System.Drawing.Point(72, 471)
+        Me.lstAverage.MultiColumn = True
+        Me.lstAverage.Name = "lstAverage"
+        Me.lstAverage.Size = New System.Drawing.Size(784, 100)
+        Me.lstAverage.TabIndex = 3
+        '
+        'lstStudents
+        '
+        Me.lstStudents.AutoSize = True
+        Me.lstStudents.Location = New System.Drawing.Point(72, 36)
+        Me.lstStudents.Name = "lstStudents"
+        Me.lstStudents.Size = New System.Drawing.Size(162, 25)
+        Me.lstStudents.TabIndex = 4
+        Me.lstStudents.Text = "Student Grades"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(72, 440)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 25)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Averages"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(945, 545)
+        Me.ClientSize = New System.Drawing.Size(945, 738)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstStudents)
+        Me.Controls.Add(Me.lstAverage)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.btnInformation)
         Me.Controls.Add(Me.ListBox1)
         Me.Name = "Form1"
         Me.Text = "Two Dimensional Array"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ListBox1 As ListBox
     Friend WithEvents btnInformation As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lstAverage As ListBox
+    Friend WithEvents lstStudents As Label
+    Friend WithEvents Label1 As Label
 End Class
--- ClevelandAuto/Form1.vb
+++ ClevelandAuto/Form1.vb
@@ -1,41 +1,60 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnInformation.Click
-        Dim intCarCount, intPropertyCount As Integer
-        Dim strHeader = "CAR" & vbTab  'Header row 
-        Dim strOutput As String ' Accumulator for outputing the car + properties
+        ListBox1.Items.Clear()
+        lstAverage.Items.Clear() 'Empty out the list boxes
 
-        'Ask user the car count and property count
+        Dim intStudentCount, intExamCount As Integer
+        Dim strHeader = "Student" & vbTab & " " 'Header row 
 
-        'An extra property is added to account for the car name
-        intPropertyCount = InputBox("Please enter the number of properties you wish to enter") + 1
-        intCarCount = InputBox("Please enter the number of cars you wish to enter") - 1
+        intStudentCount = InputBox("Please enter the number of Students") - 1 'Minus one to use an array subscript
+        intExamCount = InputBox("Please enter the number of Exams") - 1
 
+        Dim strExamTitles(intExamCount) As String      'Array for holding exam names.
+        Dim strStudentNames(intStudentCount) As String 'Array for holding student names.
 
-        Dim strProperties(intPropertyCount - 2) As String         'Holds the property strings for the input box
-        Dim strCars(intCarCount, intPropertyCount - 1) As String  'Holds the car name & properties the user puts in
-
-        Dim counter = 0
-        For Each prop In strProperties 'Make the property header, populate strProperties
-            strProperties(counter) = InputBox("Ok, enter property " & (counter + 1)).ToString.ToUpper
-            strHeader += strProperties(counter) & vbTab & "  "
-            counter += 1
+        For exam = 0 To strExamTitles.Length - 1       'Gets exam names
+            strExamTitles(exam) = InputBox("Please enter the name of Exam " & exam)
+        Next
+        For examName = 0 To strExamTitles.Length - 1   'Header row content
+            strHeader += strExamTitles(examName) & vbTab
         Next
 
-        ListBox1.Items.Add(strHeader)
+        ListBox1.Items.Add(strHeader)                  'Adds exam titles to header
+        lstAverage.Items.Add(strHeader)                'Addes exam titles to average box
 
-        'The first element of each array is the car name.
-        For car = 0 To intCarCount
-            strCars(car, 0) = InputBox("Whats the name of car #" & car + 1 & "?")
-            strOutput += strCars(car, 0) & vbTab 'Puts the car name in.
-            counter = 0                          'Used to get the property from the strProperties array.
-            For carProperty = 1 To intPropertyCount - 1 'Counter starts at 1 becuase (x,0) is the car name.
-                strCars(car, carProperty) = InputBox("Enter the " & strCars(car, 0) & " " & strProperties(counter) & " property")
-                counter += 1
-                strOutput += strCars(car, carProperty) & vbTab
+        For student = 0 To strStudentNames.Length - 1  'Gets student names
+            strStudentNames(student) = InputBox("Please enter the name of Student " & student)
+            ListBox1.Items.Add(strStudentNames(student) & vbTab + " ") 'Adds student names to listbox
+        Next
+
+        Dim strExamScores(intStudentCount, intExamCount) As String   '2d array for holding exam scores.
+        'Array for exam averages.
+
+        For student = 0 To strStudentNames.Length - 1
+            For exam = 0 To strExamTitles.Length - 1
+                strExamScores(student, exam) = InputBox("Please enter " & strStudentNames(student) & "'s score for " & strExamTitles(exam))
+
+                ListBox1.Items(student + 1) += strExamScores(student, exam) & vbTab + " "  'Adds exam scores to listbox
             Next
-            ListBox1.Items.Add(strOutput)
-            strOutput = ""
+
         Next
+
+        Dim intTotalExamGrades As Integer
+
+        Dim strAveragesString = "" & vbTab
+        Dim intExamAverage As Integer
+        For exam = 0 To strExamTitles.Length - 1
+            intTotalExamGrades = 0
+            For student = 0 To strStudentNames.Length - 1
+                intTotalExamGrades += Integer.Parse(strExamScores(student, exam))
+            Next
+            intExamAverage = intTotalExamGrades / (intExamCount + 1)
+            strAveragesString += intExamAverage.ToString() & vbTab
+        Next
+        lstAverage.Items.Add(strAveragesString)
     End Sub
 
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        My.Forms.Form1.Close()
+    End Sub
 End Class
